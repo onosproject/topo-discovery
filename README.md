a3f8d3eea65b44b712ca8ac656c1082a38bfbf7d
Adib Rastegarnia <adib.rastegarnia@intel.com>
2022-08-18 13:44:43 -0700

    Setup repo (#1)
    * setup repo
    
    * setup repo

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
+![Build](https://github.com/onosproject/topo-discovery/workflows/build/badge.svg)
+
+
+
+<!--
+SPDX-FileCopyrightText: 2022 Intel Corporation
+
+SPDX-License-Identifier: Apache-2.0
+-->
+
+
+
+
 # topo-discovery
 The subsystem for discovering network topology using P4Runtime and gNMI 
---
+++ pkg/manager/manager.go
@@ -0,0 +1,79 @@
+// SPDX-FileCopyrightText: 2022-present Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package manager
+
+import (
+	"github.com/onosproject/onos-lib-go/pkg/logging"
+	"github.com/onosproject/onos-lib-go/pkg/northbound"
+)
+
+var log = logging.GetLogger()
+
+// Config is app manager
+type Config struct {
+	CAPath      string
+	KeyPath     string
+	CertPath    string
+	TopoAddress string
+	GRPCPort    int
+	P4Plugins   []string
+}
+
+// Manager single point of entry for the device-provisioner application
+type Manager struct {
+	Config Config
+}
+
+// NewManager initializes the application manager
+func NewManager(cfg Config) *Manager {
+	log.Info("Creating application manager")
+	mgr := Manager{
+		Config: cfg,
+	}
+	return &mgr
+}
+
+// Run runs application manager
+func (m *Manager) Run() {
+	log.Info("Starting application Manager")
+
+	if err := m.start(); err != nil {
+		log.Fatal("Unable to run Manager", "error", err)
+	}
+}
+
+func (m *Manager) start() error {
+
+	err := m.startNorthboundServer()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// startSouthboundServer starts the northbound gRPC server
+func (m *Manager) startNorthboundServer() error {
+	log.Info("Starting NB server")
+	s := northbound.NewServer(northbound.NewServerCfg(
+		m.Config.CAPath,
+		m.Config.KeyPath,
+		m.Config.CertPath,
+		int16(m.Config.GRPCPort),
+		true,
+		northbound.SecurityConfig{}))
+	s.AddService(logging.Service{})
+
+	doneCh := make(chan error)
+	go func() {
+		err := s.Serve(func(started string) {
+			log.Info("Started NBI on ", started)
+			close(doneCh)
+		})
+		if err != nil {
+			doneCh <- err
+		}
+	}()
+	return <-doneCh
+}

6d001195ec8aa92c2a4d43177dcab38413aca0a7
Adib Rastegarnia <adib.rastegarnia@intel.com>
2022-09-19 16:56:10 -0700

    add a gnmi client interface (#10)

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/atomix/atomix-go-client v0.6.2
 	github.com/gogo/protobuf v1.3.2
+	github.com/golang/protobuf v1.5.2
 	github.com/onosproject/onos-api/go v0.9.45
 	github.com/onosproject/onos-lib-go v0.9.0
 	github.com/onosproject/onos-topo v0.9.7
@@ -12,6 +13,7 @@ require (
 	github.com/spf13/cobra v1.5.0
 	github.com/stretchr/testify v1.8.0
 	google.golang.org/grpc v1.48.0
+	google.golang.org/protobuf v1.28.0
 )
 
 require (
@@ -23,6 +25,7 @@ require (
 	github.com/bits-and-blooms/bitset v1.2.0 // indirect
 	github.com/bits-and-blooms/bloom/v3 v3.0.1 // indirect
 	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/cenkalti/backoff/v4 v4.1.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/eapache/go-resiliency v1.2.0 // indirect
 	github.com/eapache/go-xerial-snappy v0.0.0-20180814174437-776d5712da21 // indirect
@@ -30,8 +33,8 @@ require (
 	github.com/ericchiang/oidc v0.0.0-20160908143337-11f62933e071 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/golang/mock v1.6.0 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
@@ -44,9 +47,13 @@ require (
 	github.com/jcmturner/gokrb5/v8 v8.4.2 // indirect
 	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
 	github.com/klauspost/compress v1.14.2 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/openconfig/goyang v0.0.0-20200115183954-d0a48929f0ea // indirect
+	github.com/openconfig/grpctunnel v0.0.0-20220524190229-125331eabdde // indirect
+	github.com/openconfig/ygot v0.6.0 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.0-beta.8 // indirect
 	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
@@ -69,7 +76,6 @@ require (
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20220803205849-8f55acc8769f // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/square/go-jose.v1 v1.1.2 // indirect
 	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
---
+++ pkg/client/gnmi/client.go
@@ -0,0 +1,126 @@
+// SPDX-FileCopyrightText: 2022-present Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package gnmi
+
+import (
+	"context"
+	"github.com/golang/protobuf/proto"
+	"github.com/onosproject/onos-lib-go/pkg/logging"
+	"io"
+
+	"github.com/onosproject/onos-lib-go/pkg/errors"
+	baseClient "github.com/openconfig/gnmi/client"
+	gclient "github.com/openconfig/gnmi/client/gnmi"
+
+	gpb "github.com/openconfig/gnmi/proto/gnmi"
+)
+
+var log = logging.GetLogger()
+
+// Client gNMI client interface
+type Client interface {
+	io.Closer
+	Capabilities(ctx context.Context, r *gpb.CapabilityRequest) (*gpb.CapabilityResponse, error)
+	CapabilitiesWithString(ctx context.Context, request string) (*gpb.CapabilityResponse, error)
+	Get(ctx context.Context, r *gpb.GetRequest) (*gpb.GetResponse, error)
+	GetWithString(ctx context.Context, request string) (*gpb.GetResponse, error)
+	Set(ctx context.Context, r *gpb.SetRequest) (*gpb.SetResponse, error)
+	SetWithString(ctx context.Context, request string) (*gpb.SetResponse, error)
+	Subscribe(ctx context.Context, q baseClient.Query) error
+	Poll() error
+}
+
+// client gnmi client
+type client struct {
+	client *gclient.Client
+}
+
+// Subscribe calls gNMI subscription bacc
+// sed on a given query
+func (c *client) Subscribe(ctx context.Context, q baseClient.Query) error {
+	err := c.client.Subscribe(ctx, q)
+	go c.run(ctx)
+	return errors.FromGRPC(err)
+}
+
+// Poll issues a poll request using the backing client
+func (c *client) Poll() error {
+	return c.client.Poll()
+}
+
+func (c *client) run(ctx context.Context) {
+	log.Infof("Subscription response monitor started")
+	for {
+		err := c.client.Recv()
+		if err != nil {
+			log.Infof("Subscription response monitor stopped due to %v", err)
+			return
+		}
+	}
+}
+
+// Capabilities returns the capabilities of the target
+func (c *client) Capabilities(ctx context.Context, req *gpb.CapabilityRequest) (*gpb.CapabilityResponse, error) {
+	capResponse, err := c.client.Capabilities(ctx, req)
+	return capResponse, errors.FromGRPC(err)
+}
+
+// Get calls gnmi Get RPC
+func (c *client) Get(ctx context.Context, req *gpb.GetRequest) (*gpb.GetResponse, error) {
+	getResponse, err := c.client.Get(ctx, req)
+	return getResponse, errors.FromGRPC(err)
+}
+
+// Set calls gnmi Set RPC
+func (c *client) Set(ctx context.Context, req *gpb.SetRequest) (*gpb.SetResponse, error) {
+	setResponse, err := c.client.Set(ctx, req)
+	return setResponse, errors.FromGRPC(err)
+}
+
+// CapabilitiesWithString allows a request for the capabilities by a string - can be empty
+func (c *client) CapabilitiesWithString(ctx context.Context, request string) (*gpb.CapabilityResponse, error) {
+	r := &gpb.CapabilityRequest{}
+	reqProto := &request
+	if err := proto.UnmarshalText(*reqProto, r); err != nil {
+		return nil, errors.NewInvalid("unable to unmarshal gnmi.CapabilityRequest from %v : %v", *reqProto, err)
+	}
+	capResponse, err := c.client.Capabilities(ctx, r)
+	return capResponse, errors.FromGRPC(err)
+}
+
+// GetWithString can make a get request based on a given a string request - can be empty
+func (c *client) GetWithString(ctx context.Context, request string) (*gpb.GetResponse, error) {
+	if request == "" {
+		return nil, errors.NewInvalid("cannot get an empty request")
+	}
+	r := &gpb.GetRequest{}
+	reqProto := &request
+	if err := proto.UnmarshalText(*reqProto, r); err != nil {
+		return nil, errors.NewInvalid("unable to unmarshal gnmi getRequest from %v : %v", *reqProto, err)
+	}
+	getResponse, err := c.client.Get(ctx, r)
+	return getResponse, errors.FromGRPC(err)
+}
+
+// SetWithString can make a set request based on a given string request
+func (c *client) SetWithString(ctx context.Context, request string) (*gpb.SetResponse, error) {
+	if request == "" {
+		return nil, errors.NewInvalid("cannot set an empty request")
+	}
+	r := &gpb.SetRequest{}
+	reqProto := &request
+	if err := proto.UnmarshalText(*reqProto, r); err != nil {
+		return nil, errors.NewInvalid("unable to unmarshal gnmi set request from %v: %v", *reqProto, err)
+	}
+	setResponse, err := c.client.Set(ctx, r)
+	return setResponse, errors.FromGRPC(err)
+}
+
+// Close closes the gnmi client
+func (c *client) Close() error {
+	return c.client.Close()
+}
+
+var _ Client = &client{}
